911bea3cf0cde6784476a57b38dfecd9c71babea
EtiennePinClaret <pinclaret@gmail.com>
2020-01-21 17:40:42 +0100

    Adding print_test DynMcqInfo Et DynInfo

--- front-end dev/tests/models.py
+++ front-end dev/tests/models.py
@@ -148,6 +148,7 @@ class DynMCQInfo(models.Model):
 	id_test = models.CharField(max_length=10, primary_key=True)
 	title = models.TextField()
 	nb_q = models.CharField(max_length=10)
+	print_test = models.BooleanField(default=False)
 
 	def get_absolute_url(self):
 		return reverse('tests:Create DynMCQTest', kwargs={'input_id_test': self.id_test})
@@ -171,6 +172,7 @@ class DynMCQInfo(models.Model):
 		return reverse('tests:Launch Specific McqDyn', kwargs={'input_id_test': self.id_test})
 
 	def get_in_launch(self):
+		print_test = True
 		return reverse('tests:In Launch Specific DynMcq', kwargs={'input_id_test': self.id_test})
 		
 		
@@ -240,6 +242,7 @@ class DynTestInfo(models.Model):
 	id_test = models.CharField(max_length=10, null=False, primary_key=True)
 	title = models.CharField(max_length=10, null=False)
 	nb_q = models.CharField(max_length=10, null=False)
+	print_test = models.BooleanField(default=False)
 
 	def get_absolute_url(self):
 		return reverse('tests:Create DynTest', kwargs={'input_id_test': self.id_test})
@@ -254,6 +257,7 @@ class DynTestInfo(models.Model):
 		return reverse('tests:Launch Specific Dyn', kwargs={'input_id_test': self.id_test})
 
 	def get_in_launch(self):
+		print_test = True
 		return reverse('tests:In Launch Specific Dyn', kwargs={'input_id_test': self.id_test})
 
 			
