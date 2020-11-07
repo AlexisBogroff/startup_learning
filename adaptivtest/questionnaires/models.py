4cba81337b6c6a22968ee2071aa5ff01b5ce06ed
AlexisBogroff <alexis.bogroff@gmail.com>
2020-11-07 21:57:34 +0100

    Add docstrings

--- adaptivtest/questionnaires/models.py
+++ adaptivtest/questionnaires/models.py
@@ -1,39 +1,28 @@
+"""
+Models for the storing of data related to tests, questions, answers
+along with their corresponding categories.
+
+Classes:
+    Answers: correct and incorrect but plausible answers
+    classified by category
+
+    Categories: knowledge base of the categories with the links between
+    parent and child categories.
+
+    Parameters: static model storing constants for secondary keys
+
+    Questions: questions along with their correct and plausible answers,
+    and linked to a category
+
+    Tests: pointer to a selection of questions
+"""
+
 import uuid
 from django.db import models
 
-class Parameters(models.Model):
-    question_level_in_months_of_study = models.CharField(max_length=10)
-
-class Categories(models.Model):
-    """
-    Categories that will be used in the question model. These can be
-    anything from a general subject to a very specific domain of
-    expertise.
-
-    These categories are linked via the id of their parent category.
-    Each parent can have multiple children, thus building a knowledge 
-    base.
-
-    Args:
-        name: name of the category
-        id_parent_category: id of the single and more precise parent category
-            containing the current child category. If a category name have
-            multiple parents, it should be duplicated for each parent.
-            e.g.: 'functions' would have multiple parents 'fundamentals', 'vba',
-            'python', etc., hence one entry should exist for each of these parents,
-            so that referencing to 'functions' would require to specify which
-            'functions' we are referencing to, by using its specific id. 
-    
-    TODO:alexis(alexis.bogroff@gmail.com): reformulate id_parent_category definition
-    """
-
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=40)
-    id_parent_category = models.CharField(max_length=36)
-
 class Answers(models.Model):
     """
-    Answers that will either be used as correct answers or incorrect but 
+    Answers that will either be used as correct answers or incorrect but
     plausible answers in the question model.
 
     These answers are linked via the id of the category to which they
@@ -43,8 +32,42 @@ class Answers(models.Model):
 
     Args:
         text: the text of the question that will be seen by the learner
-        id_parent_category: 
+
+        id_parent_category:
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     text = models.TextField()
     id_parent_category = models.CharField(max_length=36)
+
+class Categories(models.Model):
+    """
+    Categories that will be used in the question model. These can be
+    anything from a general subject to a very specific domain of
+    expertise.
+
+    These categories are linked via the id of their parent category.
+    Each parent can have multiple children, thus building a knowledge
+    base.
+
+    Args:
+        name: name of the category
+
+        id_parent_category: id of the single and more precise parent category
+            containing the current child category. If a category name have
+            multiple parents, it should be duplicated for each parent.
+            e.g.: 'functions' would have multiple parents 'fundamentals', 'vba',
+            'python', etc., hence one entry should exist for each of these parents,
+            so that referencing to 'functions' would require to specify which
+            'functions' we are referencing to, by using its specific id.
+
+    TODO:alexis(alexis.bogroff@gmail.com): reformulate id_parent_category definition
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=40)
+    id_parent_category = models.CharField(max_length=36)
+
+class Parameters(models.Model):
+    """
+    TODO:alexis(alexis.bogroff@gmail.com): write docstring
+    """
+    question_level_in_months_of_study = models.CharField(max_length=10)
