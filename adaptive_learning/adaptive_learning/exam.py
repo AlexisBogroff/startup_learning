fd768c559e15642d4af0e8e08fc18941f5548b74
AlexisBogroff <alexis.bogroff@gmail.com>
2020-12-05 11:08:04 +0100

    Add create_question and create_exam functions

--- adaptive_learning/adaptive_learning/exam.py
+++ adaptive_learning/adaptive_learning/exam.py
@@ -7,6 +7,12 @@ from adaptive_learning.funcs import get_input
 def create_question():
     """
     Create a question
+
+    It is important to fill the 'keywords' argument, since it enables
+    the filtering of questions on these keywords when building exams
+
+    Returns:
+        the question dictonary
     """
     question = {}
     question['text'] = get_input("Enter the question text")
@@ -15,7 +21,8 @@ def create_question():
     question['nb_points'] = get_input("Enter the number of points")
     question['difficulty'] = get_input("Enter the difficulty")
     question['keywords'] = get_input("Enter keywords, subject, or categories")
-    question['notif_correct_answers'] = get_input("Inform on correct answers?")
+    question['notif_correct_answers'] = get_input("Inform num correct answers?")
+    question['randomize_answers_order'] = get_input("Randomize answers order?")
     return question
 
 
@@ -24,22 +31,28 @@ def create_answer():
     Create an answer
 
     Returns:
-        the different elements composing a standard answer
+        the answer dictionary
     """
     answer = {}
     answer['text'] = get_input("Enter the answer text")
-    answer['format'] = get_input("Enter the answer format")
-    answer['text'] = get_input("Is the answer correct? (True/False)")
+    answer['is_correct'] = get_input("Is the answer correct? (True/False)")
+    answer['use_answer'] = get_input("Do use this answer? (True/False)")
     return answer
 
 
 def create_exam():
     """
     Create an exam
+
+    The exam 'grade_base' enables to obtain a grade on e.g. /20, although there
+    are only e.g. 10 questions graded /1 point.
+
+    Returns:
+        the exam dictionary
     """
-    raise NotImplementedError
-
-
-if __name__ == "__main__":
-    create_question()
-    create_answer()
+    exam_ = {}
+    exam_['title'] = get_input("Enter the exam title")
+    exam_['description'] = get_input("Enter the exam description")
+    exam_['grade_base'] = get_input("Enter the grade base")
+    exam_['randomize_questions_order'] = get_input("Randomize questions order?")
+    return exam_
