36ee5c8d9d4f87b54e03084f3f5cbd0deb93607f
AlexisBogroff <alexis.bogroff@gmail.com>
2020-12-07 22:52:56 +0100

    Add a load exam method + refactoring + move exam id generation

--- adaptive_learning/adaptive_learning/exam.py
+++ adaptive_learning/adaptive_learning/exam.py
@@ -93,11 +93,17 @@ class Exam:
     Returns:
         exam instance with its basic properties set, and ready to be composed.
 
+    Notes:
+        The structure of exam has its main and secondary parameters at the same
+        level. The structure of the dump is different and may lead to useless
+        complexity and confusion, since its secondary parameters are stored in
+        a list called 'parameters'.
+
     TODO: implement set_questions function and everything that can link exams
     with questions and answers
     """
     def __init__(self):
-        self._id = generate_uuid()
+        self._id = ""
         self.title = ""
         self.description = ""
         self.questions = []
@@ -130,7 +136,7 @@ class Exam:
         return data_dump
 
 
-    def load(self, id_exam):
+    def load_exam(self, id_exam):
         """
         Load a specific exam from the exam database
 
@@ -140,99 +146,164 @@ class Exam:
         Returns:
             void.
         """
+        db = load_table_exams()
+        exam_data = self.extract_exam(id_exam, db)
+        questions = exam_data['questions']
+        self.set_properties(exam_data)
+        self.set_questions(questions)
+
+
+    def extract_exam(self, id_exam, db):
+        """
+        Extract the specific exam and apply sanity checks
+        """
+        exam_data = [exam for exam in db if exam['id'] == id_exam]
+
+        if not exam_data:
+            raise ValueError("Missing: not found with id: {}".format(id_exam))
+        if len(exam_data) > 1:
+            raise ValueError("Duplicates found for id: {}".format(id_exam))
+        if len(exam_data) == 1:
+            return exam_data[0]
+
+
+    def update(self, id_exam):
+        """
+        Update an existing exam
+
+        Notes:
+            Implement a method to find a specific id in the db file, and get the
+            corresponding row number. Then delete this row. Finally, add the
+            updated version of the exam.
+        """
         raise NotImplementedError
 
 
-    def save(self):
+    def save_new(self):
         """
-        Stores the exam in a JSON file
+        Stores the exam in database
 
         It inserts the new content at the end of the file,
-        on a new line, and takes a single line
+        on a new line, and takes a single line (each in json like format)
+
+        Method should be executed one time only, when finished with the creation
+        of the exam. Otherwise, the exam would be stored (with a new id) each
+        time save method would be called.
 
         Notes:
-            Each exam is stored in json format, but the file is in text format.
+            To increase performances, each exam is stored in json format,
+            even though the file is in text format.
             This enables to store new exams by appending them in the first empty
             rows, without the need to read and parse the whole file
             (which would be the case with a json file).
-
-        TODO: save should add the exam if does not exist, and update the
-        existing exam in the db otherwise. Implement a method to find a
-        specific id in the db file, and get the corresponding row number.
-        Then delete this row. Finally, add the new exam (updated version).
-        For now, it stores a new exam (with new id) each time the save
-        method is used. It should rather update the exam (since an other
-        instance of exam should have been constructed otherwise). For example,
-        the id of the exam could be generated when exam is instanciated. Thus,
-        there would be no additional exam with a different id when save is used
-        within a single exam.
         """
+        self._id = generate_uuid()
         exam_dump = self.get_dump()
-
-        # if already_in_db(exam_dump):
-            # update_exam_file(exam_dump)
-        # else:
         insert_to_exams_file(exam_dump)
 
 
-    def set_properties(self):
+    def set_questions(self, questions=None):
+        """ Set questions from user input or from existing """
+        if not questions:
+            self.create_question()
+        else:
+            self.load_questions(questions)
+
+
+    def load_questions(self, loaded_questions):
+        """ Load questions from exam """
+        self.questions = loaded_questions
+
+
+    def set_properties(self, exam_data=None):
         """
-        Set all the properties from user input
+        Set all the properties
+
+        Set the properties from either an existing exam, or from user input
+        if no exam is passed.
+
+        Args:
+            exam: the exam data to load if passed
 
         Returns:
             void
         """
-        self.set_title()
-        self.set_description()
-        self.set_randomize_order()
-        self.set_auto_rebase_grade()
-        self.set_grade_base()
+        if not exam_data:
+            self.set_title_from_input()
+            self.set_description_from_input()
+            self.set_randomize_order_from_input()
+            self.set_auto_rebase_grade_from_input()
+            self.set_grade_base_from_input()
+        else:
+            self.set_id_from_existing(exam_data)
+            self.set_title_from_existing(exam_data)
+            self.set_description_from_existing(exam_data)
+            self.set_randomize_order_from_existing(exam_data)
+            self.set_auto_rebase_grade_from_existing(exam_data)
+            self.set_grade_base_from_existing(exam_data)
 
 
-    def set_title(self):
+    def set_id_from_existing(self, exam_data):
+        """ Load id from existing exam """
+        self._id = exam_data['id']
+
+    def set_title_from_existing(self, exam_data):
+        """ Load title from existing exam """
+        self.title = exam_data['title']
+
+    def set_description_from_existing(self, exam_data):
+        """ Load description from existing exam """
+        self.description = exam_data['description']
+
+    def set_randomize_order_from_existing(self, exam_data):
+        """ Load randomize_questions_order parameter from existing exam """
+        param = exam_data['parameters']['randomize_questions_order']
+        self.randomize_questions_order = param
+
+    def set_auto_rebase_grade_from_existing(self, exam_data):
+        """ Load auto_rebase_grade parameter from existing exam """
+        param = exam_data['parameters']['auto_rebase_grade']
+        self.auto_rebase_grade = param
+
+    def set_grade_base_from_existing(self, exam_data):
+        """ Load grade_base parameter from existing exam """
+        param = exam_data['parameters']['grade_base']
+        self.grade_base = param
+
+
+    def set_title_from_input(self):
         """ Set title property """
         self.title = get_input("Enter the exam title")
 
-    def set_description(self):
+    def set_description_from_input(self):
         """ Set description property """
         self.description = get_input("Enter the exam description")
 
-    def set_randomize_order(self):
+    def set_randomize_order_from_input(self):
         """ Set randomize_order property """
         randomize_order = get_input("Randomize questions order?")
         casted_randomize_order = cast(randomize_order, bool)
         self.randomize_questions_order = casted_randomize_order
 
-    def set_auto_rebase_grade(self):
+    def set_auto_rebase_grade_from_input(self):
         """ Set auto_rebase_grade property """
         auto_rebase = get_input("Activate automatic grade rebasing?")
         casted_auto_rebase = cast(auto_rebase, bool)
         self.auto_rebase_grade = casted_auto_rebase
 
-    def set_grade_base(self):
+    def set_grade_base_from_input(self):
         """ Set grade_base property """
         grade_base = get_input("Enter the grade base")
         casted_grade_base = cast(grade_base, int)
         self.grade_base = casted_grade_base
 
 
-    def set_properties_from_db_load(self):
-        raise NotImplementedError
-
-    def add_question(self):
+    def create_question(self):
         """
-        Add a question to the exam list of questions property
-
-        Returns:
-            void.
-
-        TODO: Enable add from existing questions
+        Create and add a question to the exam list of questions property
         """
-        # Add a new question
         question = create_question()
         question['id'] = self.get_question_id()
-
-        # Add to exam list of questions property
         self.questions.append(question)
 
 
@@ -281,4 +352,5 @@ def load_table_exams():
 
 if __name__ == "__main__":
     exam = Exam()
-    exam.set_properties()
+    exam.load_exam("f67d625b-b569-487e-91fc-0ac14ca0bc81")
+    print(exam.questions)
