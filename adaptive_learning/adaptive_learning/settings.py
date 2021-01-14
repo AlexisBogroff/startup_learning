d8a54cfa542398032cd9e133a308d11e385e84b7
AlexisBogroff <alexis.bogroff@gmail.com>
2021-01-14 21:56:23 +0100

    Create file with global constants

---
+++ adaptive_learning/adaptive_learning/settings.py
@@ -0,0 +1,12 @@
+"""
+Main settings file
+"""
+
+# Paths to db
+__PATH_EXAMS__ = "/Users/Pro/git_repositories/"\
+    "adaptive_learning/adaptive_learning/adaptive_learning/"\
+    "data/table_exams.txt"
+
+__PATH_QUESTIONS__ = "/Users/Pro/git_repositories/"\
+    "adaptive_learning/adaptive_learning/adaptive_learning/"\
+    "data/table_questions.txt"
